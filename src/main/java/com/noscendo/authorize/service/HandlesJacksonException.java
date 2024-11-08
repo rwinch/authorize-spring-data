112345593659d901ec7141afe38b036fa30b8428
Rob Winch <362503+rwinch@users.noreply.github.com>
2024-11-08 17:19:16 -0600

    Exception handling returns view if root cause is AuthorizationDeniedException

--- src/main/java/com/noscendo/authorize/service/HandlesJacksonException.java
+++ src/main/java/com/noscendo/authorize/service/HandlesJacksonException.java
@@ -1,19 +1,33 @@
 package com.noscendo.authorize.service;
 
+import java.util.Map;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.AbstractView;
 
 @ControllerAdvice
 public class HandlesJacksonException {
 
 	@ExceptionHandler(HttpMessageNotWritableException.class)
-	ResponseEntity<?> handleWrite(HttpMessageNotWritableException ex) {
+	View handleWrite(HttpMessageNotWritableException ex) {
 		if (ex.getRootCause() instanceof AuthorizationDeniedException denied) {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+			return new AbstractView() {
+				@Override
+				protected void renderMergedOutputModel(Map<String, Object> model,
+						HttpServletRequest request, HttpServletResponse response)
+						throws Exception {
+					throw ex;
+				}
+			};
 		}
 		throw ex;
 	}
