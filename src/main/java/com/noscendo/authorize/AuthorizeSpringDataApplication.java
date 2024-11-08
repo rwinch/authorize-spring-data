ef8da0bb96aa3195db6a6ba35715b67b1b8cd24c
Rob Winch <362503+rwinch@users.noreply.github.com>
2024-11-08 17:18:15 -0600

    Init with a single user
    Can access with:
    
    http --auth user:password :8080/authorized/1

--- src/main/java/com/noscendo/authorize/AuthorizeSpringDataApplication.java
+++ src/main/java/com/noscendo/authorize/AuthorizeSpringDataApplication.java
@@ -1,7 +1,12 @@
 package com.noscendo.authorize;
 
+import com.noscendo.authorize.dao.Person;
+import com.noscendo.authorize.repo.PersonRepository;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
@@ -10,5 +15,17 @@ public class AuthorizeSpringDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizeSpringDataApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner init(PersonRepository people) {
+        return args -> {
+            Person p = new Person();
+            p.setEmail("rob@example.com");
+            p.setName("Rob");
+            p.setId(1L);
+            people.save(p);
+            people.flush();
+        };
     }
 }
